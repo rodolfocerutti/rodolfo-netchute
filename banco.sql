c63745b6985407b32476d3a3e4c20c2987fc4478
Rodolfo Cerutti <42013617+rodolfocerutti@users.noreply.github.com>
2022-03-30 20:46:41 -0300

    Arquivos adicionados - rodolfo-netchute
    SA1 Atividade 2 - Presencial - Manutenção do Sistema de Cadastros da Empresa NetChute

---
+++ banco.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clientes(
+    codigo int primary key,
+    nome varchar(255) NOT NULL,
+    cpf varchar(99) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+
+
+
+
---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b5cf4a58272915', '3305eeac', 'heroku_8583da6fc784663') or die('Erro ao conectar ao banco de dados');
